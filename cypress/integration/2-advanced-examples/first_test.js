99453f963763377cea87bc0c7dbd10498a78ba45
Grig <stepanyan.95@gmail.com>
2022-03-06 19:18:03 +0400

    remove folder

--- cypress/integration/2-advanced-examples/first_test.js
+++
@@ -1,8 +0,0 @@
-describe("google", () => {
-    it ("i can search google", () => {
-        cy.visit('https://www.google.com/')
-        cy.get("input[name='q']").type('Tesvan').type('{enter}')
-        cy.contains('https://www.tesvan.com')
-        cy.contains ('https://am.linkedin.com › tesvan')
-});
-});
